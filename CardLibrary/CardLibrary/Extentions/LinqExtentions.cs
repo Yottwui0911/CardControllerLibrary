2390240f5a18a315418ad0a2b6d6591bd544596b
TakayukiYoshizumi <t.yoshizuminunununu@gmail.com>
2019-01-22 15:38:03 +0900

    Libraryの追加

---
+++ CardLibrary/CardLibrary/Extentions/LinqExtentions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardLibrary.Extentions
+{
+    public static class LinqExtentions
+    {
+        public static IEnumerable<T> RandItems<T>(this IEnumerable<T> items)
+        {
+            return items.OrderBy(x => Guid.NewGuid());
+        }
+    }
+}
